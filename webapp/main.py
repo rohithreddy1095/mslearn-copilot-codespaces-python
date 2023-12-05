ac54dc959454d5dd5f2bfb0d544b6115c8abf046
rohithreddy1095 <rohithreddy1095@gmail.com>
2023-12-05 18:02:08 +0000

    Add endpoint for generating checksum

--- webapp/main.py
+++ webapp/main.py
@@ -18,6 +18,10 @@ class Body(BaseModel):
     length: Union[int, None] = 20
 
 
+class Text(BaseModel): 
+    text: str
+
+
 @app.get('/')
 def root():
     html_path = join(static_path, "index.html")
@@ -35,3 +39,17 @@ def generate(body: Body):
     """
     string = base64.b64encode(os.urandom(64))[:body.length].decode('utf-8')
     return {'token': string}
+
+
+# Create a FastAPI endpoint that accepts a POST request with a JSON body containing a single field called "text" and returns a checksum of the text 
+
+@app.post('/checksum')
+def checksum(body: Text):
+    """
+    Generate a checksum of the text. Example POST request body:
+
+    {
+        "text": "Hello World!"
+    }
+    """
+    return {'checksum': hash(body.text)}
